98fe31aa032e1074ce829aeb7b9d49ee63ac1bc6
denningkevin <denningkev@gmail.com>
2019-12-17 08:41:32 +0300

    add butterknife and overrides to main activity

--- app/src/main/java/com/mulama/beer_superior/MainActivity.java
+++ app/src/main/java/com/mulama/beer_superior/MainActivity.java
@@ -2,13 +2,39 @@ package com.mulama.beer_superior;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.viewBeersButton) Button mViewBeersButton;
+    @BindView(R.id.enterNameEditText) EditText mEnterNameEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mViewBeersButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mViewBeersButton) {
+            String name = mEnterNameEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, BeerActivity.class);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        }
+    }
+
+
 }
