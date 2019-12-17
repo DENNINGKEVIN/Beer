ee24109c4471225e4bc092a0fb19c9a6570d49e7
denningkevin <denningkev@gmail.com>
2019-12-17 09:27:49 +0300

    create submit functionality in contribute activity

--- app/src/main/java/com/mulama/beer_superior/ContributeActivity.java
+++ app/src/main/java/com/mulama/beer_superior/ContributeActivity.java
@@ -2,13 +2,23 @@ package com.mulama.beer_superior;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class ContributeActivity extends AppCompatActivity {
+public class ContributeActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contribute);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(ContributeActivity.this, BeerActivity.class);
+        startActivity(intent);
+
     }
 }
