4710e888d9cee54239679ba82c2e0d8e03b5f6a3
denningkevin <denningkev@gmail.com>
2019-12-17 08:47:58 +0300

    create MyBeerArrayAdapter

--- app/src/main/java/com/mulama/beer_superior/MyBeerArrayAdapter.java
+++ app/src/main/java/com/mulama/beer_superior/MyBeerArrayAdapter.java
@@ -1,4 +1,30 @@
 package com.mulama.beer_superior;
 
-public class MyBeerArrayAdapter {
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyBeerArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mBeers;
+    private Integer[] mRatings;
+
+    public MyBeerArrayAdapter (Context mContext,int resource,String[] mBeers,Integer[] mRatings){
+        super(mContext,resource);
+        this.mContext=mContext;
+        this.mBeers=mBeers;
+        this.mRatings=mRatings;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String beer = mBeers[position];
+        Integer rating = mRatings[position];
+        return String.format("%s \n Rated: %s", beer, rating);
+    }
+
+    @Override
+    public int getCount(){
+        return mBeers.length;
+    }
 }
