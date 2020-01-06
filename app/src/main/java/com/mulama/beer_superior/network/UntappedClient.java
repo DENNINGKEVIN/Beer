bb1984e292e7af5078fd4f6709dbd72f0a2b090e
denningkevin <denningkev@gmail.com>
2020-01-06 11:18:09 +0300

    edit untapped api adding get request

--- app/src/main/java/com/mulama/beer_superior/network/UntappedClient.java
+++ app/src/main/java/com/mulama/beer_superior/network/UntappedClient.java
@@ -1,16 +1,4 @@
 package com.mulama.beer_superior.network;
 
-import com.mulama.beer_superior.models.UntappedBeerInfoSearchResponse;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-
-public interface UntappedClient {
-
-    @GET("beer_info/search")
-    Call<UntappedBeerInfoSearchResponse> getRestaurants(
-            @Query("location") String location,
-            @Query("term") String term
-    );
+public class UntappedClient {
 }
